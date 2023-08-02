15ca8dcf6781b2aa591dd3143f679a9c9d547f5c
kaytdehoyos <kaytdehoyos@gmail.com>
2023-08-02 17:55:22 -0600

    testing

---
+++ src/Main2.java
@@ -0,0 +1,12 @@
+public class Main2 {
+    public static void main(String[] args) {
+        boolean productAvailable=true;
+
+        if (productAvailable) {
+            System.out.println("yes");
+
+        } else {
+            System.out.println("no");
+        }
+    }
+}
